2d8c4a5251c80f3756fefb620e44d16504a53116
mechcarbide <alicejian0313@gmail.com>
2025-06-19 17:02:52 +0800

    Update auth-github.js

--- functions/auth-github.js
+++ functions/auth-github.js
@@ -14,7 +14,7 @@ export async function onRequest(context) {
         const url = new URL(request.url);
         const redirectUrl = new URL('https://github.com/login/oauth/authorize');
         redirectUrl.searchParams.set('client_id', client_id);
-        redirectUrl.searchParams.set('redirect_uri', url.origin + '/api/callback');
+        redirectUrl.searchParams.set('redirect_uri', url.origin + '/auth-callback');
         redirectUrl.searchParams.set('scope', 'repo user');
         redirectUrl.searchParams.set(
             'state',
